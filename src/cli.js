4331c28f134af876e595dbb79a89ae2025d2d9f9
Wolfgang E. Sanyer <WolfgangESanyer@gmail.com>
2021-09-14 11:46:56 -0400

    js: create target subdir when not present
    Signed-off-by: Wolfgang E. Sanyer <WolfgangESanyer@gmail.com>

--- src/cli.js
+++ src/cli.js
@@ -1,5 +1,6 @@
 import * as clih from "./cli-helpers.js"
 import * as ebuildh from "./ebuild-helpers.js"
+import fs from "fs";
 
 export async function cli() {
   const target = clih.getTargetFilename();
@@ -8,13 +9,20 @@ export async function cli() {
     return;
   }
   else {
+    const subdir = `dev-js`;
     console.log(`Ok, generating ebuild(s) for ${target}`);
+    console.log(`Targetting subdir ${subdir}`)
 
 
     // const deps = await clih.getDependencies(target);
     // deps.forEach(({ pkg, ver }) => console.log(`    ${pkg}: ${ver}`))
     const pkmnt = await clih.getPackument(target);
     const ebuild = await ebuildh.makeEbuild(pkmnt, "6.26.2");
+
+    if (!fs.existsSync(subdir)) {
+      console.log(`creating subdir ${subdir}`)
+      fs.mkdirSync(subdir);
+    }
 
     console.log(ebuild)
   }
